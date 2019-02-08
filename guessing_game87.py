7473b91f57d4c9ad66980e67a026af86523b3fc6
unknown <rpetchen@gmu.edu>
2019-02-08 14:44:55 -0500

    first commit

---
+++ guessing_game87.py
@@ -0,0 +1,50 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+
+For this first project we will be using Workspaces. 
+
+NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
+
+"""
+
+import random
+
+
+def start_game():
+    guess_count = 0
+    random_num = random.randint(1, 10)
+
+    guess_is_number = False
+    print(random_num)
+    print("Welcome to the number guessing game!\n\n")
+
+    while guess_is_number != True:
+
+        try:
+            user_guess = int(input("Please enter a number between 1 and 10: \n"))
+        except ValueError:
+            print("Please enter only whole numbers!\n")
+        print(user_guess)
+
+        if user_guess < random_num:
+            print ("It's lower\n")
+            guess_count += 1
+        elif user_guess > random_num:
+            print("It's higher")
+            guess_count += 1
+        else:
+            guess_count += 1
+            print("Got it")
+            print("It took {} tries to guess correctly\n".format(guess_count))
+            guess_is_number = True
+
+    print("Game is now ending, goodbye!")
+
+
+
+
+if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.
+    start_game()
