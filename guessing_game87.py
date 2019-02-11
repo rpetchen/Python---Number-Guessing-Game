0935834c966bde7f0f49816331ce0d678c588db4
unknown <rpetchen@gmu.edu>
2019-02-11 08:20:08 -0500

    fixes

--- guessing_game87.py
+++ guessing_game87.py
@@ -21,24 +21,24 @@ def start_game():
     print("Welcome to the number guessing game!\n\n")
 
     while guess_is_number != True:
-
+        user_guess = ""
         try:
             user_guess = int(input("Please enter a number between 1 and 10: \n"))
         except ValueError:
             print("Please enter only whole numbers!\n")
-        print(user_guess)
 
-        if user_guess < random_num:
-            print ("It's lower\n")
-            guess_count += 1
-        elif user_guess > random_num:
-            print("It's higher")
-            guess_count += 1
-        else:
-            guess_count += 1
-            print("Got it")
-            print("It took {} tries to guess correctly\n".format(guess_count))
-            guess_is_number = True
+        if isinstance(user_guess, int):
+            if user_guess > random_num:
+                print ("It's lower\n")
+                guess_count += 1
+            elif user_guess < random_num:
+                print("It's higher")
+                guess_count += 1
+            else:
+                guess_count += 1
+                print("Got it")
+                print("It took {} tries to guess correctly\n".format(guess_count))
+                guess_is_number = True
 
     print("Game is now ending, goodbye!")
 
